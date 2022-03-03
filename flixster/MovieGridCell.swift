37dd77cc3d51137f3000823f3685da62e5de791a
chrisyifanjin <chris.yifanjin@gmail.com>
2022-03-03 09:36:09 -0800

    navigation+layout
    the phase 2 of my project

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Yifan Jin on 3/3/2022.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
